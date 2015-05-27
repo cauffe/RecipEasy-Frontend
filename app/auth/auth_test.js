fca845e5b75d54607ab2a5750f35e67995176245
Nate McNeil <nhmcneil@gmail.com>
2015-05-27 17:51:15 -0600

    added tests for app.js

--- app/auth/auth_test.js
+++ app/auth/auth_test.js
@@ -5,12 +5,12 @@ describe('myApp.auth module', function () {
 	beforeEach(module('myApp.auth'));
 
 	describe('LoginCtrl controller', function () {
-		var scope;
-		var ctrl;
+		var $scope, ctrl, User;
 
-		beforeEach(inject(function($rootScope, $controller) {
-			scope = $rootScope.$new();
-			ctrl = $controller('LoginCtrl', {$scope: scope});
+		beforeEach(inject(function($controller, _User_) {
+			$scope = {};
+			User = _User_;
+			ctrl = $controller('LoginCtrl', {$scope: $scope, User: User});
 		}));
 
 		it('should have controller defined', function () {
@@ -18,25 +18,27 @@ describe('myApp.auth module', function () {
 		});
 
 		it('should have $scope.credentials set to empty object', function(){
-			expect(scope.credentials).toEqual({});
+			expect($scope.credentials).toEqual({});
 		});
 
 		it('should have $scope.credentials set to test credentials', function(){
-			scope.credentials = {
+			$scope.credentials = {
 				username: 'test',
 				password: 'test'
 			};
 
-			expect(scope.credentials).toEqual({
+			expect($scope.credentials).toEqual({
 				username: 'test',
 				password: 'test'
 			});
 		});
 
-		it('should have call the User.login function and set user data', inject(function(_User_, $httpBackend){
-			scope.credentials = {username: 'test', password: 'test'};
+		it('$scope.login() should have called the User.login() function', inject(function($q, $httpBackend){
+			var deferredSuccess = $q.defer();
 
-			$httpBackend.whenPOST('http://localhost:8001/login').respond({
+			$scope.credentials = {username: 'test', password: 'test'};
+
+			$httpBackend.whenPOST('http://localhost:8001/obtain-auth-token/').respond({
 				status: 200,
 				data: {
 					token: '0984313451098345',
@@ -44,37 +46,41 @@ describe('myApp.auth module', function () {
 				}
 			});
 
-			scope.login();
-			// expect(scope.credentials).toEqual({}); TODO: Flesh this test out more
+			spyOn(User, 'login').andReturn(deferredSuccess.promise);
+
+			$scope.login();
+
+			expect(User.login).toHaveBeenCalled()
+
 		}));
 
 		it('should have $scope.alerts set to empty array', function(){
-			expect(scope.alerts).toEqual([]);
+			expect($scope.alerts).toEqual([]);
 		});
 
 		it('should have $scope.alerts set to test string', function(){
-			scope.alerts.push('Alert Test');
+			$scope.alerts.push('Alert Test');
 
-			expect(scope.alerts.length).toEqual(1);
-			expect(scope.alerts[0]).toEqual('Alert Test');
+			expect($scope.alerts.length).toEqual(1);
+			expect($scope.alerts[0]).toEqual('Alert Test');
 		});
 
 		it('should have the alert removed from array', function(){
-			scope.alerts.push('Alert Test');
-			expect(scope.alerts.length).toEqual(1);
-			scope.closeAlert(0);
-			expect(scope.alerts.length).toEqual(0);
+			$scope.alerts.push('Alert Test');
+			expect($scope.alerts.length).toEqual(1);
+			$scope.closeAlert(0);
+			expect($scope.alerts.length).toEqual(0);
 		})
 
 	});
 
 	describe('RegistrationCtrl controller', function () {
-		var scope;
-		var ctrl;
+		var $scope, ctrl, User;
 
-		beforeEach(inject(function($rootScope, $controller) {
-			scope = $rootScope.$new();
-			ctrl = $controller('RegistrationCtrl', {$scope: scope});
+		beforeEach(inject(function($controller, _User_) {
+			$scope = {};
+			User = _User_;
+			ctrl = $controller('RegistrationCtrl', {$scope: $scope, User: User});
 		}));
 
 		it('should have controller defined', function () {
@@ -82,25 +88,25 @@ describe('myApp.auth module', function () {
 		});
 
 		it('should have $scope.user_info set to empty object', function(){
-			expect(scope.user_info).toEqual({});
+			expect($scope.user_info).toEqual({});
 		});
 
 		it('should have $scope.alerts set to empty array', function(){
-			expect(scope.alerts).toEqual([]);
+			expect($scope.alerts).toEqual([]);
 		});
 
 		it('should have $scope.alerts set to test string', function(){
-			scope.alerts.push('Alert Test');
+			$scope.alerts.push('Alert Test');
 
-			expect(scope.alerts.length).toEqual(1);
-			expect(scope.alerts[0]).toEqual('Alert Test');
+			expect($scope.alerts.length).toEqual(1);
+			expect($scope.alerts[0]).toEqual('Alert Test');
 		});
 
 		it('should have the alert removed from array', function(){
-			scope.alerts.push('Alert Test');
-			expect(scope.alerts.length).toEqual(1);
-			scope.closeAlert(0);
-			expect(scope.alerts.length).toEqual(0);
+			$scope.alerts.push('Alert Test');
+			expect($scope.alerts.length).toEqual(1);
+			$scope.closeAlert(0);
+			expect($scope.alerts.length).toEqual(0);
 		})
 
 	});
