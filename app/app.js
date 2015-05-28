94531eb9e69b60e9065ed292c1cdb882d3379726
Nate McNeil <nhmcneil@gmail.com>
2015-05-28 11:17:26 -0600

    update license and app.js

--- app/app.js
+++ app/app.js
@@ -3,9 +3,9 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
     'ngRoute',
+	'myApp.auth',
     'myApp.view1',
-    'myApp.view2',
-    'myApp.auth'
+    'myApp.view2'
 ])
 
     .config(['$routeProvider', function($routeProvider) {
